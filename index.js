e7c45cd11ad1515ed584f319d83f84f6da5b9b19
AQUELEFelipe1 <127846083+AQUELEFelipe1@users.noreply.github.com>
2024-08-29 01:28:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const primeiroNome = "Felipe";
+const sobrenome = "Medeiros";
+const idade = 17;
+const altura = 1.64;
+const massa = 47;
+const cidadeNatal = "São Paulo";
+
+console.log("MEU NOME É:");
+console.log(primeiroNome);
+console.log(sobrenome);
+console.log("MINHA IDADE É:");
+console.log(idade);
+console.log("MINHA ALTURA É:");
+console.log(altura);
+console.log("MINHA MASSA É:");
+console.log(massa);
+console.log("EU NASCI EM:");
+console.log(cidadeNatal);
+console.log("HABILIDADE CONQUISTADA KK:");
+console.log("Um dos primeiros que zerou Resident Evil 4 só com granadas")
